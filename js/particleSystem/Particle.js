89f95acd826e959d40f303399b19030b0103b492
Jarrod Overson <jsoverson@gmail.com>
2011-06-16 10:26:43 -0700

    cleanup

--- js/particleSystem/Particle.js
+++ js/particleSystem/Particle.js
@@ -7,23 +7,16 @@ function Particle(point,velocity){
 	this.lived        = 0;
 }
 
+Particle.globalDrawColor = "#437EDE";
+
 Particle.prototype.submitToFields = function(fields) {
-//   var totalAcceleration = new Vector(0,0);
    var totalAccelerationX = 0;
    var totalAccelerationY = 0;
    
    for (var i = 0; i < fields.length; i++) {
       var field = fields[i];
 
-//      var vector = this.position.vectorTo(field.position);
-//      var magnitude = vector.getMagnitude();
-//      var force = field.mass / (magnitude*magnitude*magnitude); // Gravitational pull * ratio of pull to distance f=(m1m2/r^2) & ratio=(f/r)
-//      var acceleration = new Vector(
-//         vector.x * force,
-//         vector.y * force
-//      );
-
-      // inlining for performance
+      // inlining what should be Vector object methods for performance reasons
       var vectorX = field.position.x - this.position.x;
       var vectorY = field.position.y - this.position.y;
       var force = field.mass / Math.pow(vectorX*vectorX+vectorY*vectorY,1.5); 
@@ -32,16 +25,12 @@ Particle.prototype.submitToFields = function(fields) {
    }
    this.acceleration = new Vector(totalAccelerationX,totalAccelerationY);
 };
-//Particle.globalDrawColor = "rgba(166,67,0,.7)";
-Particle.globalDrawColor = "#437EDE";
 
 Particle.prototype.move = function() {
    this.velocity.x += this.acceleration.x;
    this.velocity.y += this.acceleration.y;
    this.position.x += this.velocity.x;
    this.position.y += this.velocity.y;
-//   this.velocity.add(this.acceleration);
-//   this.position.add(this.velocity);
 };
 
 Particle.prototype.drawSize = function(display) {
